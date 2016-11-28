e47ff517c3ed860cdefaedd04190ed5a6a0df97d
Stefan du Fresne <stefan@sdufresne.info>
2016-11-28 18:48:12 +0000

    Making migrations compatible with CouchDB 2.0 medic/medic-webapp#2855
    * Removing temp view usage medic/medic-webapp#2855
    * Nodify _config usage medic/medic-webapp#2855
    
    We're just going to presume one node here, because they are migrations
    that are run once, and are solving legacy problems that will only have
    existed on that first local node.
    
    If we ever start running a setup with multiple nodes, none of which are
    on the same box as medic-api, we may have to revisit this.

--- migrations/remove-empty-parents.js
+++ migrations/remove-empty-parents.js
@@ -1,14 +1,5 @@
 var db = require('../db');
 
-var emptyParentsView = function(doc) {
-  if (['district_hospital', 'health_center', 'clinic', 'person'].indexOf(doc.type) === -1 ||
-      typeof doc.parent === 'undefined' ||
-      (doc.parent && Object.keys(doc.parent).length)) {
-    return;
-  }
-  emit();
-};
-
 module.exports = {
   name: 'remove-empty-parents',
   created: new Date(2016, 7, 10, 13, 37, 0, 0),
@@ -16,19 +7,34 @@ module.exports = {
     db.request({
       db: 'medic',
       method: 'POST',
-      path: '_temp_view',
-      body: { map: emptyParentsView.toString() },
-      qs: {
-        include_docs: true,
+      path: '_find',
+      body: {
+        selector: {
+          type: {
+            $in: ['district_hospital', 'health_center', 'clinic', 'person']
+          },
+          $or: [
+          {
+            parent: {
+              $eq: null
+            }
+          },
+          {
+            parent: {
+              $exists: true,
+              _id: {
+                $exists: false
+              }
+            }
+          }]
+        }
       }
     }, function(err, result) {
       if (err) {
         return callback(err);
       }
 
-      var docs = result.rows.map(function(row) {
-        return row.doc;
-      });
+      var docs = result.docs;
 
       docs.forEach(function(doc) {
         delete doc.parent;
