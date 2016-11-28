aa6593a9dc24ed1bde7d1843759a501931415242
Stefan du Fresne <stefan@sdufresne.info>
2016-11-28 18:48:12 +0000

    Making migrations compatible with CouchDB 2.0 medic/medic-webapp#2855
    * Removing temp view usage medic/medic-webapp#2855
    * Nodify _config usage medic/medic-webapp#2855
    
    We're just going to presume one node here, because they are migrations
    that are run once, and are solving legacy problems that will only have
    existed on that first local node.
    
    If we ever start running a setup with multiple nodes, none of which are
    on the same box as medic-api, we may have to revisit this.

--- migrations/convert-bad-dob-format.js
+++ migrations/convert-bad-dob-format.js
@@ -1,15 +1,6 @@
 var db = require('../db'),
     moment = require('moment');
 
-var map = function(doc) {
-  if (doc.type === 'person' && doc.date_of_birth && doc.date_of_birth.indexOf(' ') >= 0) {
-    emit(1);
-  }
-};
-var temporaryView = {
-  'map': map.toString()
-};
-
 module.exports = {
   name: 'convert-bad-dob-format',
   created: new Date(2016, 4, 20),
@@ -17,15 +8,17 @@ module.exports = {
     db.request({
       db: 'medic',
       method: 'POST',
-      path: '_temp_view',
-      body: temporaryView,
-      qs: {
-        include_docs: true,
+      path: '_find',
+      body: {
+        selector: {
+          type: 'person',
+          date_of_birth: {
+            $regex: ' '
+          }
+        }
       }
     }, function(err, result) {
-      var docs = result.rows.map(function(row) {
-        return row.doc;
-      });
+      var docs = result.docs;
 
       docs.forEach(function(doc) {
         doc.date_of_birth = moment(doc.date_of_birth, 'MMM Do, YYYY').format('YYYY-MM-DD');
