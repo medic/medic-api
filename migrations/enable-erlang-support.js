949a98306acefed1a38421e1ae3451f044bf0fdf
Alex Anderson <anderson@medicmobile.org>
2016-11-30 16:11:28 +0100

    Use COUCH_NODE_NAME env var instead of hard-coded couch node name
    Issue: medic/medic-webapp#2933

--- migrations/enable-erlang-support.js
+++ migrations/enable-erlang-support.js
@@ -7,7 +7,7 @@ module.exports = {
     db.request({
       db: '_node',
       method: 'PUT',
-      path: 'couchdb@localhost/_config/native_query_servers/erlang',
+      path: process.env.COUCH_NODE_NAME + '/_config/native_query_servers/erlang',
       body: '{couch_native_process, start_link, []}'
     }, function(err) {
       callback(err);
