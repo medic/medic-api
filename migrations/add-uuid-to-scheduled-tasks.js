7e787258b6d2a9c27cdb1c7cffd1d74160d707fb
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2017-01-05 13:56:55 +1300

    Migration for adding UUIDs to messages. medic/medic-webapp#3015

---
+++ migrations/add-uuid-to-scheduled-tasks.js
@@ -0,0 +1,84 @@
+var db = require('../db'),
+    async = require('async'),
+    moment = require('moment'),
+    uuid = require('node-uuid'),
+    BATCH_SIZE = 100;
+
+var updateMessage = function(message) {
+  if (message.uuid) {
+    // already has the required uuid
+    return false;
+  }
+  message.uuid = uuid.v4();
+  return true;
+};
+
+var updateTask = function(task) {
+  return moment().isBefore(moment(task.due)) &&
+         task.messages.some(updateMessage);
+};
+
+var update = function(row) {
+  return row.doc &&
+         row.doc.type === 'data_record' &&
+         row.doc.form &&
+         row.doc.scheduled_tasks &&
+         row.doc.scheduled_tasks.some(updateTask);
+};
+
+var save = function(docs, callback) {
+  if (!docs.length) {
+    return callback();
+  }
+  db.medic.bulk({ docs: docs }, callback);
+};
+
+var runBatch = function(skip, callback) {
+  var options = {
+    include_docs: true,
+    limit: BATCH_SIZE,
+    skip: skip
+  };
+  db.medic.list(options, function(err, result) {
+    if (err) {
+      return callback(err);
+    }
+    console.log('        Processing ' + skip + ' to ' + (skip + BATCH_SIZE) + ' docs of ' + result.total_rows + ' total');
+    var toSave = result.rows.filter(update).map(function(row) {
+      return row.doc;
+    });
+    save(toSave, function(err) {
+      var keepGoing = result.total_rows > (skip + BATCH_SIZE);
+      callback(err, keepGoing);
+    });
+  });
+};
+
+module.exports = {
+  name: 'add-uuid-to-scheduled-tasks',
+  created: new Date(2017, 1, 5),
+  run: function(callback) {
+    db.getSettings(function(err, data) {
+      if (err) {
+        return callback(err);
+      }
+      var schedules = data.settings.schedules;
+      if (!schedules ||
+          !schedules.length ||
+          (schedules.length === 1 && !schedules[0].name)) {
+        return callback();
+      }
+      var currentSkip = 0;
+      async.doWhilst(
+        function(callback) {
+          runBatch(currentSkip, callback);
+        },
+        function(more) {
+          currentSkip += BATCH_SIZE;
+          return more;
+        },
+        callback
+      );
+    });
+  }
+};
