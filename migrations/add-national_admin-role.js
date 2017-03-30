a01463496424ace5f44c6ee43bc36dc5996078d8
Stefan du Fresne <stefan@sdufresne.info>
2017-03-30 14:38:15 +0100

    Allow admining on medic as well
    Issue medic/medic-webapp#3262

--- migrations/add-national_admin-role.js
+++ migrations/add-national_admin-role.js
@@ -1,29 +1,34 @@
-const db = require('../db');
+const _ = require('underscore'),
+      db = require('../db'),
+      async = require('async');
+
+const addRole = (dbname, role, callback) => db.request({
+    db: dbname,
+    method: 'GET',
+    path: '_security'
+  }, (err, result) => {
+    if (err) {
+      return callback(err);
+    }
+
+    if (!result.admins.roles.includes(role)) {
+      console.log(`Adding ${role} role to ${dbname} admins`);
+      result.admins.roles.push(role);
+    }
+
+    db.request({
+      db: dbname,
+      method: 'PUT',
+      path: '_security',
+      body: result
+    }, callback);
+  });
 
 module.exports = {
   name: 'add-national_admin-role',
   created: new Date(2017, 3, 30),
-  run: callback => {
-    db.request({
-      db: '_users',
-      method: 'GET',
-      path: '_security'
-    }, (err, result) => {
-      if (err) {
-        return callback(err);
-      }
-
-      if (!result.admins.roles.includes('national_admin')) {
-        console.log('Adding "national_admin" role to _user admins');
-        result.admins.roles.push('national_admin');
-      }
-
-      db.request({
-        db: '_users',
-        method: 'PUT',
-        path: '_security',
-        body: result
-      }, callback);
-    });
-  }
+  run: callback => async.series([
+    _.partial(addRole, '_users', 'national_admin'),
+    _.partial(addRole, 'medic', 'national_admin')
+  ], callback)
 };
