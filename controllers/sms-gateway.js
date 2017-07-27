cb698f09044433d2ff06cefa490d706d404f64be
Henok Getachew Alemayehu <henygetachew@gmail.com>
2017-07-27 09:17:08 +0300

    Make sure callback always gets called.

--- controllers/sms-gateway.js
+++ controllers/sms-gateway.js
@@ -23,13 +23,15 @@ const _ = require('underscore'),
 // See: https://github.com/medic/medic-webapp/issues/3019
 // Specifically, this should be in a new repo that we can pull in via npm
 function saveToDb(message, callback) {
-  if(message.from && message.content){
-    recordUtils.createByForm({
-      from: message.from,
-      message: message.content,
-      gateway_ref: message.id,
-    }, callback);
+  if(!message.from || !message.content){
+    return callback();
   }
+
+  recordUtils.createByForm({
+    from: message.from,
+    message: message.content,
+    gateway_ref: message.id,
+  }, callback);
 }
 
 function mapStateFields(update) {
