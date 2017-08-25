a6a0c4729e1d2c7829f8177165b038135bf340fe
Alex Anderson <anderson@medicmobile.org>
2017-08-25 10:01:00 +0000

    Update get method usage

--- tests/e2e/controllers/sms-gateway.spec.js
+++ tests/e2e/controllers/sms-gateway.spec.js
@@ -6,7 +6,7 @@ describe('SMS Gateway API', () => {
   beforeEach(() => utils.cleanDb());
 
   it('should respond to GET requests to show the URL is correct', () =>
-    utils.apiRequest('/api/sms')
+    utils.apiGet('/api/sms')
       .then(result => assert.equal(result, 'show me the result')));
   
 });
