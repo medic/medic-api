0f5753fc653e6737ddcebde93a1738301e9e5fa2
Alex Anderson <anderson@medicmobile.org>
2017-08-25 09:51:13 +0000

    Fix jshint

--- tests/e2e/controllers/sms-gateway.spec.js
+++ tests/e2e/controllers/sms-gateway.spec.js
@@ -7,6 +7,6 @@ describe('SMS Gateway API', () => {
 
   it('should respond to GET requests to show the URL is correct', () =>
     utils.apiRequest('/api/sms')
-      .then(result => assert.equal(result, 'show me the result'));
+      .then(result => assert.equal(result, 'show me the result')));
   
 });
