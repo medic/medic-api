26eb65608c04461c1a0b54a6ddd9ec609c6343c9
Alex Anderson <anderson@medicmobile.org>
2017-08-25 10:05:07 +0000

    Update assertion to check for correct json

--- tests/e2e/controllers/sms-gateway.spec.js
+++ tests/e2e/controllers/sms-gateway.spec.js
@@ -5,8 +5,8 @@ describe('SMS Gateway API', () => {
 
   beforeEach(() => utils.cleanDb());
 
-  it('should respond to GET requests to show the URL is correct', () =>
+  it('should respond to GET requests to show the endpoint supports the gateway API', () =>
     utils.apiGet('/api/sms')
-      .then(result => assert.equal(result, 'show me the result')));
+      .then(result => assert.isTrue(result['medic-gateway'])));
   
 });
